60ab7b46c6a0240deef2bfaf94fbc5d6d8992d47
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2020-09-07 01:27:07 -0700

    Update formatting

--- ch3_p1-3.js
+++ ch3_p1-3.js
@@ -12,23 +12,14 @@ const min = function(a,b) {
 
 const isEven = function(n) {
     if (n === 0 ) {
-
         return true;
-
     } else if (n === 1) {
-
         return false;
-
     } else if (n < 0) {
-
         return isEven((n * -1) -2);
-
     } else {
-
 	return isEven(n - 2);
-
     }
-
 };
 
 
